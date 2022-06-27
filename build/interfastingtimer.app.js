47264f3db73820169030ec57a3f1f1d1567d7fa5
TheLogan <timlogan475@gmail.com>
2022-06-27 22:36:58 +0200

    first commit

---
+++ build/interfastingtimer.app.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*
+* Features
+* Start times
+* End times
+* Eat days
+* Permanent face icon (can eat or only water)
+*/
+// const myAsync = async () => {
+//   const {hello, world} = {hello: 'hello', world: 'world'};
+//   console.log(hello);
+//   await new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       return resolve(true);
+//     }, 1000);
+//   })
+//   console.log(world);
+// }
+// myAsync();
+
+/******/ })()
+;
